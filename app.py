706080f59eb1cdd71f7f821d8f1dd463bdc6acda
Samuel Colvin <s@muelcolvin.com>
2016-04-07 21:25:43 +0100

    add ps info

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
 from pathlib import Path
+import asyncio
 
 from aiohttp import web
 import aiohttp_jinja2
 import jinja2
 
 
+async def get_ps():
+
+    create = asyncio.create_subprocess_exec('ps', '-auxf', stdout=asyncio.subprocess.PIPE)
+    proc = await create
+
+    data = await proc.stdout.read()
+    text = data.decode('ascii')
+
+    await proc.wait()
+    return text
+
+
 @aiohttp_jinja2.template('index.jinja')
 async def handle(request):
     with open('/proc/cpuinfo') as f:
-        output = f.read()
-    return {'output': output}
+        return {
+            'cpuinfo': f.read(),
+            'psauxf': await get_ps()
+        }
 
 
 app = web.Application()
